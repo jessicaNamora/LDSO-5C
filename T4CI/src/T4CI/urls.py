fa758bf8f18d6c375370eb85ee0eb9dbe3ec98b6
Teresa Matos <teresamatos1042@gmail.com>
2015-11-30 17:51:57 +0000

    Corrigido o redirect da pagina mydreams

--- T4CI/src/T4CI/urls.py
+++ T4CI/src/T4CI/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
@@ -10,7 +11,8 @@ urlpatterns = [
     url(r'^contact/$', 'newsletter.views.contact', name='contact'),
     url(r'^about/$', 'newsletter.views.about', name='about'),
     url(r'^profile/$', 'newsletter.views.profile', name='profile'),
-    url(r'^mydreams/$', 'T4CI.views.dreams', name='mydreams'),
+    url(r'^mydreams/$','T4CI.views.dreams', name='mydreams'),
+    url(r'mydreams/$', RedirectView.as_view(url='/mydreams', permanent=False), name='mydreams'),
     url(r'^team/(?P<dream_id>[0-9]+)$', 'T4CI.views.team', name='team'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^dream/(?P<dream_id>[0-9]+)/$', 'T4CI.views.dream', name='dream'),
